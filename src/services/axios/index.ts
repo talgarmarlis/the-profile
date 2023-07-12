eff8479810153eaa31efb4173b96086e395098bf
Talgar Marlis Uulu <talguulu@amazon.lu>
2023-07-12 16:40:54 +0200

    extending to 5 mins

--- src/services/axios/index.ts
+++ src/services/axios/index.ts
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 const apiClient = axios.create({
     baseURL: 'https://notion-api-proxy.talgarmarlis.workers.dev',
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         'Content-Type': 'application/json'
     },
