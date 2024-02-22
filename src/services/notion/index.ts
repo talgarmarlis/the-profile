25dd411a22b50601576fbd8ca44e2502b5ceb460
Talgar <talgarmarlis@gmail.com>
2024-02-22 21:18:16 +0100

    reverting back filter

--- src/services/notion/index.ts
+++ src/services/notion/index.ts
@@ -1,24 +1,16 @@
 import apiClient from "../axios";
 
-function queryDatabase(database_id: any, data: any){
+function queryDatabase(database_id: any){
     // return notionFetch("POST", `databases/${database_id}/query`, {});
-    return apiClient.post(`databases/${database_id}/query`, data)
+    return apiClient.post(`databases/${database_id}/query`)
 }
 
 function getArticleList() {
-    return queryDatabase('bd581e1674cf4fbc8bce782d67e7492e', {})
+    return queryDatabase('bd581e1674cf4fbc8bce782d67e7492e')
 }
 
 function getLatestCompendiumPages() {
-    return queryDatabase('44e0a82fca2444f384bcc83d6e17e760', 
-        {
-            filter: {
-                Finalized: {
-                    equals: true
-                }
-            }
-        }
-    )
+    return queryDatabase('44e0a82fca2444f384bcc83d6e17e760')
 }
 
 function getPage(pageId: string) {
