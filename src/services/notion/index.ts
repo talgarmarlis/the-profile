f6fe42fa9a06a67930ae329a2bccc6f3ccff4d68
Talgar <talgarmarlis@gmail.com>
2025-03-18 10:39:34 +0100

    updating compendium pages

--- src/services/notion/index.ts
+++ src/services/notion/index.ts
@@ -1,42 +1,42 @@
 import apiClient from "../axios";
 
-function queryDatabase(database_id: any, filterData?: any){
-    // return notionFetch("POST", `databases/${database_id}/query`, {});
-    return apiClient.post(`databases/${database_id}/query`, filterData)
+function queryDatabase(database_id: any, filterData?: any) {
+  // return notionFetch("POST", `databases/${database_id}/query`, {});
+  return apiClient.post(`databases/${database_id}/query`, filterData);
 }
 
 function getArticleList() {
-    return queryDatabase('bd581e1674cf4fbc8bce782d67e7492e')
+  return queryDatabase("bd581e1674cf4fbc8bce782d67e7492e");
 }
 
 function getLatestCompendiumPages() {
-    return queryDatabase('44e0a82fca2444f384bcc83d6e17e760', {
-        filter: {
-            property: "Completed Date",
-            date: {
-                is_not_empty: true
-            }
-        },
-        sorts: [
-            {
-                property: "Completed Date",
-                direction: "descending"
-            }
-        ]
-    })
+  return queryDatabase("1ba1d77a4f1c80738717c96853c6e427", {
+    filter: {
+      property: "Completed Date",
+      date: {
+        is_not_empty: true,
+      },
+    },
+    sorts: [
+      {
+        property: "Completed Date",
+        direction: "descending",
+      },
+    ],
+  });
 }
 
 function getPage(pageId: string) {
-    return apiClient.get(`pages/${pageId}`)
+  return apiClient.get(`pages/${pageId}`);
 }
 
 function getBlocks(blockId: string) {
-    return apiClient.get(`blocks/${blockId}/children`)
+  return apiClient.get(`blocks/${blockId}/children`);
 }
 
 export const notionService = {
-    getArticleList,
-    getLatestCompendiumPages,
-    getPage,
-    getBlocks
+  getArticleList,
+  getLatestCompendiumPages,
+  getPage,
+  getBlocks,
 };
